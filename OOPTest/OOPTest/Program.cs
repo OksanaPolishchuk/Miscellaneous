8518c4407a122338f0c37c20465188ddbad8de68
OksanaPolishchuk <polishchukoksana.s@gmail.com>
2021-03-13 13:09:32 +0200

    Polymorphism

--- OOPTest/OOPTest/Program.cs
+++ OOPTest/OOPTest/Program.cs
@@ -7,7 +7,32 @@ namespace OOPTest
     {
         static void Main(string[] args)
         {
-                
+            int i = 0;  //Int32 j = new Int32Converter();
+
+            Person person = new Person();
+            person._firstName = "Oksana";
+            person._lastName = "Polishhcuk";
+
+            Person person2 = new Person();
+            person2._firstName = "Anton";
+            person2._lastName = "Ahmat";
+
+            Doctor doctor = new Doctor();
+            doctor._firstName = "Dima";
+            doctor._lastName = "Go";
+            doctor.Income();
+            doctor.Specialization = "medium";
+
+            //polymorphism.
+            person = doctor; // Any doctor's specialization.
+            Console.WriteLine(person._firstName); // Doctor's Name
+            Console.WriteLine(person._lastName);
+            Doctor ddoctor = (Doctor)person; // Returned the doctor's specialization
+
+            Console.WriteLine(ddoctor._firstName);
+            Console.WriteLine(ddoctor.Specialization);
+
+            Console.WriteLine();
         }
     }
 }
